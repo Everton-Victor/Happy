44320903184f1e192a62f85ba5347b12da21d567
Everton Victor <evertonvictor2019br@gmail.com>
2020-10-15 17:41:53 -0300

    retirado as terminações .html e colocado a url relativa ./

--- public/scripts/page-orphanages.js
+++ public/scripts/page-orphanages.js
@@ -21,7 +21,7 @@ const popup = L.popup({
     className: 'map-popup',
     minWidth: 240,
     minHeight: 240
-}).setContent('Lar das meninas <a href="orphanage.html?id=1" class="choose-orphanage"><img src="/images/arrow-white.svg"></a>')
+}).setContent('Lar das meninas <a href="/orphanage?id=1" class="choose-orphanage"><img src="/images/arrow-white.svg"></a>')
 
 
 // Create and add marker
